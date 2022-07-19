8639f6b501da385893f8067e06ee83342bb74d96
lukeg <lukegorman@gmail.com>
2022-07-19 15:01:40 +0100

    add additional handling

--- nvsvocprez/authentication/login.py
+++ nvsvocprez/authentication/login.py
@@ -28,20 +28,51 @@ async def login(request: Request):
     return await oauth.auth0.authorize_redirect(request, redirect_uri)
 
 
+# Authentication exceptions
+class MissingRoleException(Exception):
+    """Exception raised when user lacks the correct role in Auth0."""
+
+
+class NotVerifiedException(Exception):
+    """Exception raised when user hasn't verified their email address."""
+
+async def process_auth(request: Request):
+    """Process callback coming from Auth0."""
+    # handles response from token endpoint
+    token = await oauth.auth0.authorize_access_token(request)
+    access_token = token["access_token"]
+    id_token = token["id_token"]
+    user = token.get("userinfo").json()
+
+    # Check that user is valid before proceeding
+    if not user.get("email_verified", False):
+        raise NotVerifiedException
+    roles = user.get(OAUTH_ROLES_NAMESPACE + "/roles", [])
+    if USER_ROLE not in roles:
+        raise MissingRoleException
+
+    request.session["access_token"] = access_token
+    request.session["id_token"] = id_token
+    request.session["jwt_payload"] = user
+    request.session["profile"] = {"user_id": user["sub"], "name": user["name"], "picture": user["picture"]}
+
 @router.get("/auth")
 @router.get("/auth/")
 async def auth(request: Request):
-    token = await oauth.auth0.authorize_access_token(request)
-    user = token.get("userinfo")
-    if user:
-        request.session["user"] = user
-    # Check that user is valid before proceeding
-    if not user.get("email_verified", False):
-        return RedirectResponse(url="/")
-    roles = user.get(OAUTH_ROLES_NAMESPACE + "/roles", [])
-    if USER_ROLE not in roles:
-        return RedirectResponse(url="/")
-    return RedirectResponse(url="/")
+    try:
+        request = await process_auth(request= Request)
+    except NotVerifiedException:
+        error_url = "/login-error?error=not_verified"
+    except MissingRoleException:
+        error_url = "/login-error?error=missing_role"
+    except Exception as exc:
+        print("Authentication error:")
+        print(exc)
+        error_url = "/login-error"
+    if error_url:
+        # this logout step is necessary to clear the session from both app + auth0 sides
+        return logout(request)
+    return RedirectResponse("/dashboard")
 
     
 @router.get("/logout")
