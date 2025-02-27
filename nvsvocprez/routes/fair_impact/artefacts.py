689fc1fd03f4c96fe3bda9bed076f89d1644eff1
qtl <qtl@noc.ac.uk>
2025-02-27 10:13:26 +0000

    add artefactsID route

--- nvsvocprez/routes/fair_impact/artefacts.py
+++ nvsvocprez/routes/fair_impact/artefacts.py
@@ -42,125 +42,25 @@ artefacts_context = {
     "@language": "en",
 }
 
-
 @router.get("/artefacts", **paths["/artefacts"]["get"])
 @router.head("/artefacts", include_in_schema=False)
 def artefacts(request: Request):
-
-    graph_items = []
-
     # Collections
     with httpx.Client(follow_redirects=True) as client:
         response = client.get("http://vocab.nerc.ac.uk/collection?_mediatype=application/ld+json&_profile=nvs")
 
     data = response.json()
-
     graph_collection_items = get_collection_graph_items(data)
-
-    # for item in data.get("@graph", []):
-    #     uri = item.get('@id')
-
-    #      # Determine status
-    #     status = "production"
-    #     if "DEPRECATED" in item.get("skos:prefLabel", "") or "DEPRECATED" in item.get("dc:title", ""):
-    #         status = "deprecated"
-
-    #     date_str = item.get("dc:date")
-    #     date_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f")
-
-    #     bibliographic_citation = (
-    #         f"[British Oceanographic Data Centre, year {date_obj.year}, "
-    #         f"{item.get('dc:title')}, version {item.get('owl:versionInfo')}, "
-    #         f"{item.get('dc:publisher')}, "
-    #         f"{uri} accessed on {date.today()}"
-    #         f"]"
-    #     )
-
-    #     graph_items.append(
-    #     {
-    #         "@acronym": extract_collection_acronym(uri),
-    #         "accessRights": "public",
-    #         "subject": ["TBD"],
-    #         "URI": uri,
-    #         "creator": [item.get('dc:creator')],
-    #         "identifier": uri,
-    #         "status": status,
-    #         "language": ["en"],
-    #         "rightsHolder": item.get('dc:creator'),
-    #         "license": "https://creativecommons.org/licenses/by/4.0/",
-    #         "description": item.get('dc:description'),
-    #         "landingPage": uri,
-    #         "keyword": [""],
-    #         "bibliographicCitation": bibliographic_citation,
-    #         "contactPoint": ["vocab.services@bodc.ac.uk"],
-    #         "publisher": [item.get('dc:publisher')],
-    #         "createdWith": ["https://github.com/RDFLib/VocPrez"],
-    #         "accrualPeriodicity": "http://purl.org/cld/freq/daily",
-    #         "includedInDataCatalog": ["http://vocab.nerc.ac.uk/"],
-    #         "@id": uri,
-    #         "@type": ["https://w3id.org/mod#SemanticArtefact","http://www.w3.org/2004/02/skos/core#Collection"],
-    #         "links": {
-    #             "distributions": uri.replace("collection","artefacts").replace("/current/","/distributions"),
-    #             }
-    #         }
-    #     )
 
     # Schemes
     with httpx.Client(follow_redirects=True) as client:
         response = client.get("http://vocab.nerc.ac.uk/scheme?_mediatype=application/ld+json&_profile=nvs")
 
     data = response.json()
-
     graph_scheme_items = get_scheme_graph_items(data)
-
-    # for item in data.get("@graph", []):
-    #     uri = item.get('@id')
-
-    #     date_str = item.get("dc:date")
-    #     date_obj = datetime.strptime(date_str["@value"], "%Y-%m-%dT%H:%M:%S")
-
-    #     bibliographic_citation = (
-    #         f"[British Oceanographic Data Centre, year {date_obj.year}, "
-    #         f"{item.get('dc:title')}, version {item.get('owl:versionInfo')}, "
-    #         f"{item.get('dc:publisher')}, "
-    #         f"{uri} accessed on {date.today()}"
-    #         f"]"
-    #     )
-
-    #     graph_items.append(
-    #     {
-    #         "acronym": extract_scheme_acronym(uri),
-    #         "accessRights": "public",
-    #         "subject": [],
-    #         "URI": uri,
-    #         "creator": [item.get('dc:creator')],
-    #         "identifier": [uri],
-    #         "status": "production",
-    #         "language": ["en"],
-    #         "rightsHolder": item.get('dc:creator'),
-    #         "description": item.get('dc:description'),
-    #         "landingPage": uri,
-    #         "keyword": [""],
-    #         "bibliographicCitation": bibliographic_citation,
-    #         "contactPoint": ["vocab.services@bodc.ac.uk"],
-    #         "publisher": [item.get('dc:publisher')],
-    #         "createdWith": ["https://github.com/RDFLib/VocPrez"],
-    #         "includedInDataCatalog": ["http://vocab.nerc.ac.uk/"],
-    #         "@id": uri,
-    #         "@type": ["https://w3id.org/mod#SemanticArtefact","http://www.w3.org/2004/02/skos/core#ConceptScheme"],
-    #         "links": {
-    #         "distributions": uri.replace("scheme","artefacts").replace("/current/","/distributions"),
-    #         }
-    #     }
-    #     )
-
-    print(type(graph_scheme_items))
-    print(type(graph_collection_items))
-
     json_ld = {"@context": artefacts_context, "@graph": graph_collection_items + graph_scheme_items}
 
     return JSONResponse(content=json_ld, status_code=response.status_code)
-
 
 @router.get("/artefacts/{artefactID}", **paths["/artefacts/{artefactID}"]["get"])
 @router.head("/artefacts/{artefactID}", include_in_schema=False)
@@ -179,39 +79,45 @@ def artefactId(request: Request, artefactID: str):
         return JSONResponse(content={"error": "artefactID not found"}, status_code=404)
 
     a_context = artefacts_context
-
     json_ld = {}
-    graph_items = []
 
     if response_collection.status_code == 200:
-        a_context["@vocab"] = collection_uri
-
+        a_context["@vocab"] = collection_uri        
         data = response_collection.json()
         graph_items = get_collection_graph_items(data)
-        graph_item = next((item for item in graph_items if item["@id"] == collection_uri), None)
-
-        json_ld = {"@context": a_context}
-        json_ld.update(graph_item)
-
+        graph_item = next((item for item in graph_items if item["@id"] == collection_uri), None)        
     else:
-        print("A Scheme")
-        a_context["@vocab"] = "http://vocab.nerc.ac.uk/scheme/"
+        a_context["@vocab"] = scheme_uri
+        data = response_scheme.json()
+        graph_item = next((item for item in data["@graph"] if item["@id"] == scheme_uri), None)
+
+        graph_item = get_scheme_graph_items(
+            {
+            "@graph": [graph_item]
+            }
+        )[0]
+
+    json_ld = {"@context": a_context}
+    json_ld.update(graph_item)
 
     return JSONResponse(content=json_ld, status_code=200)
 
 
-def extract_collection_acronym(url):
-    match = re.search(r"/collection/(.*?)/current/", url)
+def extract_collection_acronym(uri):
+    match = re.search(r"/collection/(.*?)/current/", uri)
     return match.group(1)
 
-
-def extract_scheme_acronym(url):
-    match = re.search(r"/scheme/(.*?)/current/", url)
+def extract_scheme_acronym(uri):
+    match = re.search(r"/scheme/(.*?)/current/", uri)
     return match.group(1)
 
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str["@value"], "%Y-%m-%dT%H:%M:%S")
+    except (ValueError, TypeError, KeyError):
+        return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f")
 
 def get_collection_graph_items(data: dict):
-
     graph_items = []
 
     for item in data.get("@graph", []):
@@ -263,17 +169,13 @@ def get_collection_graph_items(data: dict):
 
     return graph_items
 
-
 def get_scheme_graph_items(data: dict):
-
     graph_items = []
-
+    
     for item in data.get("@graph", []):
-        uri = item.get("@id")
-
-        date_str = item.get("dc:date")
-        date_obj = datetime.strptime(date_str["@value"], "%Y-%m-%dT%H:%M:%S")
-
+        uri = item.get("@id")        
+        date_obj = parse_date(item.get("dc:date"))
+        
         bibliographic_citation = (
             f"[British Oceanographic Data Centre, year {date_obj.year}, "
             f"{item.get('dc:title')}, version {item.get('owl:versionInfo')}, "
